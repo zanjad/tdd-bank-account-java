0d5367a9740b3a4d097dfa6c94e80c3165b9820c
amolzanjad <1Asnnwpowy>
2022-01-28 18:08:13 +0530

    Remove the classes

--- src/main/java/org/xpdojo/bank/Account.java
+++
@@ -1,4 +0,0 @@
-package org.xpdojo.bank;
-
-public class Account {
-}
--- src/main/java/org/xpdojo/bank/Money.java
+++
@@ -1,8 +0,0 @@
-package org.xpdojo.bank;
-
-/**
- * Immutable class to represent Money as a concept.
- * This class should have no accessor methods.
- */
-public class Money {
-}
