31839fe8d27063fe2407aa7c9d5439b969f984ef
amolzanjad <1Asnnwpowy>
2022-01-28 18:30:21 +0530

    Added empty Account

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,6 +1,5 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,8 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+	public void initialAccount_shouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
     }
 }
